64d1c6a96cd2a979ef20d9723a2f6aeade196109
Vaibhav Mokale <80054985+Vaibhav-sarvashreshth@users.noreply.github.com>
2026-04-19 18:44:40 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
+    {
+        /*
+        k ---> length of the longes string of strs
+        TC ---> O(n*k*log(k))
+        SC ---> O(n*k)
+        */
+        unordered_map<string , vector<string>>mp;
+        for(int i=0;i<strs.size();i++)
+        {
+            string temp = strs[i];
+            sort(temp.begin() , temp.end());
+            mp[temp].push_back(strs[i]);
+        }
+        vector<vector<string>>ans;
+
+        for(auto it : mp)
+        {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+        
+    }
+};
